95f4c38fb22115481a8aed9c2d66765494370e16
unknown <oscar88hh@hotmail.com>
2021-06-23 22:57:42 -0500

    Pruebas realizadas con results

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -3,19 +3,13 @@ describe('Buscar elementos', ()=> {
         cy.visit('/'); // toma el sitio padre el especificado en baseUrl
     })
     it('Buscar elementos con multiples resultados', function(){
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('dress');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('dress');
         cy.fixture('searchResult').then((searchResult)=>{
             cy.get(searchResult.title).should('contain','dress')
         })
     })
     it('Buscar por elementos sin resultados', ()=>{
-        cy.fixture('index').then((index)=>{
-            cy.get(index.searchBox).type('qwert');
-            cy.get(index.searchButton).click();
-        })
+        cy.search('qwerty');
         cy.fixture('searchResult').then((searchResult)=>{
             cy.get(searchResult.alert).should('contain','No results')
         })
